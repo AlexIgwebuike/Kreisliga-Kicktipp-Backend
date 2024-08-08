5ed7cfaf28d908801bb7e7b37f359945ac42385a
Alex-Igwebuike <alex.igwebuike@imaralabs.com>
2024-08-08 11:24:55 +0200

    renamed the database_connection to database_client

---
+++ database/database_client.go
@@ -0,0 +1,66 @@
+package database
+
+import (
+	"context"
+	"log"
+	"os"
+	"sync"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var clientIstance *mongo.Client
+var clientIstanceError error
+var databaseClientSingleton sync.Once
+
+func loadDatabaseUriFromEnv() string {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env File")
+	}
+
+	mongoDBUri := os.Getenv("MONGODB_URI")
+
+	return mongoDBUri
+}
+
+func CreateDatabaseClient() (*mongo.Client, error) {
+	databaseClientSingleton.Do(func() {
+		databaseUri := loadDatabaseUriFromEnv()
+
+		client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(databaseUri))
+
+		if err != nil {
+			clientIstanceError = err
+			log.Printf("Error %v", clientIstanceError)
+
+			return
+		}
+
+		clientIstance = client
+
+	})
+
+	return clientIstance, clientIstanceError
+}
+
+func CreatUniqueIndex(collection *mongo.Collection, field string) error {
+	indexModel := mongo.IndexModel{
+		Keys:    bson.M{field: 1},
+		Options: options.Index().SetUnique(true),
+	}
+
+	_, err := collection.Indexes().CreateOne(context.TODO(), indexModel)
+	return err
+}
+
+func DisconnectDatabaseClient() error {
+	if clientIstance != nil {
+		clientIstance.Disconnect(context.TODO())
+	}
+
+	return nil
+}
