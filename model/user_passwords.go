c1f900c926ba738fad9c78fff3bbf580f9b91746
Alex-Igwebuike <alex.igwebuike@imaralabs.com>
2024-08-08 10:19:26 +0200

    added struct tags to the models to prevent double ID's

--- model/user_passwords.go
+++ model/user_passwords.go
@@ -3,7 +3,7 @@ package model
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type UserPassword struct {
-	ID             primitive.ObjectID
+	ID             primitive.ObjectID `bson:"_id, omitempty"`
 	UserID         primitive.ObjectID
 	HashedPassword string
 }
