f6e97e6c95378d92691f23cab14a38418bfde5f4
Alex-Igwebuike <alex.igwebuike@imaralabs.com>
2024-08-09 11:00:38 +0200

    Refactor: optimized the code to only call the run function in the main function

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/AlexIgwebuike/Kreisliga-Kicktipp/database"
 	"github.com/AlexIgwebuike/Kreisliga-Kicktipp/server"
 	"github.com/AlexIgwebuike/Kreisliga-Kicktipp/service"
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -14,21 +15,53 @@ var databaseClient *mongo.Client
 var databaseClientError error
 
 func main() {
+	if err := run(); err != nil {
+		log.Fatalf("Application failed: %v", err)
+	}
+}
 
-	//Setup Database
+func run() error {
+	if err := LoadEnvFile(); err != nil {
+		return err
+	}
+
+	if err := initDatabase(); err != nil {
+		return err
+	}
+
+	initServer()
+
+	return nil
+}
+
+func LoadEnvFile() error {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env File")
+		return err
+	}
+
+	return nil
+}
+
+func initDatabase() error {
 	databaseClient, databaseClientError = database.CreateDatabaseClient()
 
 	if databaseClientError != nil {
-		log.Fatalf("Failed to Connect to Database %v", databaseClientError)
+		log.Printf("Failed to Connect to Database %v", databaseClientError)
 	}
 
 	log.Println("Connected Successfully to Database")
 
 	service.SetupDatabaseUserService(databaseClient)
 
-	//Start Echo Server
-	echoServer := server.CreateEchoServer()
-	controller.SetupUserRoutes(echoServer)
-	server.StartEchoServer(echoServer)
+	return nil
+}
 
+func initServer() {
+	echoServer := server.CreateEchoServer()
+
+	controller.SetupUserRoutes(echoServer)
+
+	server.StartEchoServer(echoServer)
 }
